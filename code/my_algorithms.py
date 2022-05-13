0ab84bebc0a3f1bb43c4ed49fbf313052f213453
Michael Bryant <m.alexander.bryant@gmail.com>
2022-05-13 15:42:20 -0700

    upload simple linear regression

---
+++ code/my_algorithms.py
@@ -0,0 +1,84 @@
+"""A compilation of machine learning algorithms that I've built from scratch."""
+
+import numpy as np
+
+class SimpleLinearRegression:
+    
+    """
+    Simple linear regression using gradient descent.
+    """
+    
+    def __init__(self):
+        
+        self.theta=np.zeros(2)
+        self.theta0_history=list()
+        self.theta1_history=list()
+        self.cost_history=list()
+            
+    
+    def compute_cost(self, X, y, theta):
+        """
+        Compute cost for linear regression. Computes the cost of using theta as the
+        parameter for linear regression to fit the data points in X and y.
+        """
+        
+        m = y.size  
+        cost = 0
+
+        cost = np.sum(np.power((np.dot(X,theta) - y),2))/(2*m)
+        
+        return cost
+    
+    def gradient_descent(self, X, y, alpha, num_iters):
+        """
+        Performs gradient descent to learn `theta`. Updates theta by taking `num_iters`
+        gradient steps with learning rate `alpha`.        
+        """
+        
+        m = y.size
+        theta = self.theta.copy()
+        theta0_history = self.theta0_history.copy()
+        theta1_history = self.theta1_history.copy()
+        cost_history = self.cost_history.copy()
+        for i in range(num_iters):
+            dJdt = np.dot((np.dot(X,theta) - y).T, X)/m
+            theta = theta - alpha*dJdt
+            theta0_history.append(theta[0])
+            theta1_history.append(theta[1])
+            cost_history.append(self.compute_cost(X, y, theta))
+
+        return theta, theta0_history, theta1_history, cost_history
+    
+    def fit(self, X, y, alpha=0.0001, num_iters=2000):
+        
+        """
+        Fit the training data.
+        
+        Input:
+        X.shape: (m,).
+        y.shape: (m,).
+        alpha: learning rate.
+        num_iters: number of iterations.
+        """
+        
+        X=np.stack([np.ones(X.size), X], axis=1)
+        
+        theta, theta0_history, theta1_history, cost_history = self.gradient_descent(X,y,alpha,num_iters)
+        self.theta = theta
+        self.theta0_history = theta0_history
+        self.theta1_history = theta1_history
+        self.cost_history = cost_history
+        
+    def predict(self,X):
+        """
+        Predict using calculated theta.
+        
+        Input:
+        X.shape: (m,).
+        
+        Output:
+        y.size: m
+        """
+        y_pred = np.dot(np.stack([np.ones(X.size), X], axis=1), self.theta)
+        
+        return y_pred
---
+++ code/simple_linear_regression_test.py
@@ -0,0 +1,42 @@
+import numpy as np
+from my_algorithms import SimpleLinearRegression
+import matplotlib.pyplot as plt
+
+# Set random seed.
+np.random.seed(0)
+
+# Generate linear data with noise.
+X = np.linspace(0,100,100)
+delta = np.random.normal(-2,3,size=(100,))
+y = .4*X + 3 + delta
+
+# Create an instance of SimpleLinearRegression and fit training data.
+lr = SimpleLinearRegression()
+lr.fit(X,y)
+
+# Plot of data with best fit line.
+plt.scatter(X,y)
+plt.plot(X,lr.predict(X),"r")
+plt.savefig('../output/simple_linear_regression/scatter_best_fit.jpg', bbox_inches='tight')
+plt.show()
+
+# Plot of cost against iterations.
+plt.plot(np.arange(0,len(lr.cost_history)),lr.cost_history)
+plt.savefig('../output/simple_linear_regression/plot_cost_iterations.jpg', bbox_inches='tight')
+plt.show()
+
+# Plot of cost against theta0.
+plt.plot(lr.theta0_history,lr.cost_history)
+plt.savefig('../output/simple_linear_regression/plot_theta0_cost.jpg', bbox_inches='tight')
+plt.show()
+
+# Plot of cost against theta1.
+plt.plot(lr.theta1_history,lr.cost_history)
+plt.savefig('../output/simple_linear_regression/plot_theta1_cost.jpg', bbox_inches='tight')
+plt.show()
+
+# 3D plot of thetas (x,y) and cost (z).
+ax = plt.axes(projection='3d')
+ax.scatter3D(lr.theta0_history, lr.theta1_history, lr.cost_history)
+plt.savefig('../output/simple_linear_regression/scatter3d_theta0_theta1_cost.jpg', bbox_inches='tight')
+plt.show()
